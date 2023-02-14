2fbd7ed62f2108ab7c0a48b4da92497b781e5c7c
Echo2124 <echo2124@hotmail.com>
2023-02-15 02:39:35 +1100

    initialise project repo

---
+++ target/debug/deps/qudelix_cli-920e35ee296a57df.d
@@ -0,0 +1,5 @@
+/Users/joshua/Documents/qudelix-cli/target/debug/deps/qudelix_cli-920e35ee296a57df: src/main.rs
+
+/Users/joshua/Documents/qudelix-cli/target/debug/deps/qudelix_cli-920e35ee296a57df.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/qudelix-cli.d
@@ -0,0 +1,1 @@
+/Users/joshua/Documents/qudelix-cli/target/debug/qudelix-cli: /Users/joshua/Documents/qudelix-cli/src/main.rs
